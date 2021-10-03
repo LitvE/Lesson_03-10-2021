f57fdad8c7747aeb359c749f7d90ba719e871703
LitvE <yevgen.litvinov1980@gmail.com>
2021-10-03 12:13:43 +0300

    Modules

---
+++ assets/modul1/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+export function sum(value1, value2) {
+  return value1 + value2;
+}
+
+export function sub(value1, value2) {
+    return value1 - value2;
+}
+
+export function mult(value1, value2) {
+    return value1 * value2;
+}
+
+export function div(value1, value2) {
+    return value1 / value2;
+}
---
+++ assets/modul2/index.js
@@ -0,0 +1,19 @@
+'use strict';
+/*
+import { sum } from '../modul1/index.js';
+import { sub } from '../modul1/index.js';
+import { mult } from '../modul1/index.js';
+import { div } from '../modul1/index.js';*/
+
+import * as calc from '../modul1/index.js';
+
+/*
+console.log(sum(2, 5));
+console.log(sub(2, 5));
+console.log(mult(2, 5));
+console.log(div(2, 5));*/
+
+console.log(calc.sum(2, 5));
+console.log(calc.sub(2, 5));
+console.log(calc.mult(2, 5));
+console.log(calc.div(2, 5));
